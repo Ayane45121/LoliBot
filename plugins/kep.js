293e35a638bdc74abb50bc254fad942643a57e81
Ayane45121 <108357375+Ayane45121@users.noreply.github.com>
2022-07-08 23:53:22 +0700

    Update kep.js

--- plugins/kep.js
+++ plugins/kep.js
@@ -5,13 +5,13 @@ let kasar = fs.readFileSync('./mp3/.WhatsApp-Audio-2021-04-01-at-14.11.16.opus')
 conn.sendFile(m.chat, kasar, '', '', m, true)
 }
 
-handler.customPrefix = /^(asu|bangsat|babi|anjing|anjim|bokep|setan)$/i
+handler.customPrefix = /^(asu|bangsat|babi|anjing|matamu|bokep|memek)$/i
 handler.command = new RegExp
 
 handler.limit = 10
 handler.mods = false 
 handler.premium = false
 handler.group = true
-handler.private = true
+handler.private = false
 
 module.exports = handler
